aee2c95604c9a8e430a47773f9eb1851823186e5
Brandon R. Beck <brandonrbeck101@gmail.com>
2018-09-24 08:21:12 -0500

    Autocomplete Usernames working
    To use, type @SomeRecentlySeenUsername.
    It remembers the last 200 viewed posts ATM. Displaying any matching 
    username, sorted by
    
    1) Usernames starting with what you typed before ones just containing.
    2) Users from the current thread before other threads.
    3) Users who have @mentioned you before those who have not.

--- common.js
+++ common.js
@@ -74,6 +74,9 @@ function getSnapshotByXpath(path,node=document) {
 function getItterByXpath(path,node=document) {
   return document.evaluate( path.toString(), node, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
 }
+function getOrderedItterByXpath(path,node=document) {
+  return document.evaluate( path.toString(), node, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+}
 function getElementByXpath(path,node=document) {
   return document.evaluate( path.toString(), node, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
@@ -129,6 +132,19 @@ function XPath(xpath_str="") {
   xp.getItter = function(node=document) {
     return getItterByXpath(xp, node);
   };
+  xp.getOrderedItter = function(node=document) {
+    return getOrderedItterByXpath(xp, node);
+  };
+  xp.forEachElement = (fn,node) => {
+    for(let [i,item] = [xp.getItter()]; (()=>{item=i.iterateNext(); return item;})();) {
+      fn(item);
+    };
+  }
+  xp.forEachOrderedElement = (fn,node) => {
+    for(let [i,item] = [xp.getOrderedItter()]; (()=>{item=i.iterateNext(); return item;})();) {
+      fn(item);
+    };
+  }
   return xp;
 }
 
@@ -168,9 +184,11 @@ function getUserValues(keys,fn) {
   let values={};
   let itemsLeft=Object.keys(keys).length;
   Object.entries(keys).forEach( ([key,defaultValue]) => {
+    dbg("in loop");
+    let jsonDefault = JSON.stringify(defaultValue);
     if (typeof GM === "object" && typeof GM.getValue === 'function') {
-      GM.getValue(key,defaultValue).then((value) => {
-        values[key] = value;
+      GM.getValue(key,jsonDefault).then((value) => {
+        values[key] = JSON.parse(value);
         itemsLeft--;
         if (itemsLeft === 0 ) {
           fn(values);
@@ -178,7 +196,69 @@ function getUserValues(keys,fn) {
       });
     }
     else if (typeof GM_getValue === 'function') {
-      values[key] = GM_getValue(key,defaultValue);
+      values[key] = JSON.parse(GM_getValue(key,jsonDefault));
+      itemsLeft--;
+      if (itemsLeft === 0 ) {
+        fn(values);
+      }
+    }
+    else {
+      dbg("To use 'getUserValues' you must grant either GM.getValue or GM_getValue.");
     }
   });
 }
+function setUserValues(keys,fn=()=>{return true;}) {
+  let values={};
+  let itemsLeft=Object.keys(keys).length;
+  Object.entries(keys).forEach( ([key,newValue]) => {
+    if (typeof GM === "object" && typeof GM.setValue === 'function') {
+      GM.setValue(key,JSON.stringify(newValue)).then(() => {
+        itemsLeft--;
+        if (itemsLeft === 0 ) {
+          fn();
+        }
+      });
+    }
+    else if (typeof GM_setValue === 'function') {
+      GM_setValue(key,JSON.stringify(newValue));
+      itemsLeft--;
+      if (itemsLeft === 0 ) {
+        fn();
+      }
+    }
+    else {
+      dbg("To use 'setUserValues' you must grant either GM.setValue or GM_setValue.");
+    }
+  });
+}
+
+const keycodes={
+  backspace:8,    tab:9,         enter:13,
+  shift:16,       ctrl:17,       alt:18,
+  pause_break:19, capslock:20,   escape:27,
+  space:32,       pageup:33,     pagedown:34,
+  end:35,         home:36,       leftarrow:37,
+  uparrow:38,     rightarrow:39, downarrow:40,
+  insert:45,      delete:46,
+  0:48,   1:49,   2:50,   3:51,
+  4:52,   5:53,   6:54,   7:55,
+  8:56,   9:57,   a:65,   b:66,
+  c:67,   d:68,   e:69,   f:70,
+  g:71,   h:72,   i:73,   j:74,
+  k:75,   l:76,   m:77,   n:78,
+  o:79,   p:80,   q:81,   r:82,
+  s:83,   t:84,   u:85,   v:86,
+  w:87,   x:88,   y:89,   z:90,
+  multiply: 106, add: 107, subtract: 109,
+  decimalpoint: 110, divide: 111,
+  f1: 112, f2: 113, f3: 114,
+  f4: 115, f5: 116, f6: 117,
+  f7: 118, f8: 119, f9: 120,
+  f10: 121, f11: 122, f12: 123,
+  numlock: 144, scrolllock: 145,
+  semicolon: 186, equalsign: 187,
+  comma: 188, dash: 189, period: 190,
+  forwardslash: 191, graveaccent: 192,
+  openbracket: 219, backslash: 220,
+  closebraket: 221, singlequote: 222
+};
