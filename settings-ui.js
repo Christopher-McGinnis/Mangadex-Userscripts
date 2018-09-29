d1bed91842da2156978a37607ab039c97eec3825
Brandon R. Beck <brandonrbeck101@gmail.com>
2018-09-28 23:27:01 -0500

    Cleanup Quicksettings interface

--- settings-ui.js
+++ settings-ui.js
@@ -1,19 +1,20 @@
 // ==UserScript==
 // @name     Mangadex Settings
 // @version  0.0.1
-// @description Settings UI builder for userscripts. Should be required by other userscripts.
+// @description Settings UI builder for Mangadex userscripts. Should be required by other userscripts.
 // @grant    unsafeWindow
 // @grant    GM.getValue
 // @grant    GM.setValue
 // @grant    GM_getValue
 // @grant    GM_setValue
 // @require  https://cdn.rawgit.com/Brandon-Beck/Mangadex-Userscripts/c2f35786a2a72ffbc37a104f5f720e1fb4c41854/common.js
-// @require  https://cdn.rawgit.com/component/textarea-caret-position/af904838644c60a7c48b21ebcca8a533a5967074/index.js
 // @match    https://mangadex.org/*
 // @author   Brandon Beck
 // @icon     https://mangadex.org/images/misc/default_brand.png?1
 // @license  MIT
 // ==/UserScript==
+// Note, above metablock is only processed when installed directly
+// Done for debugging purposes
 "use strict";
 
 function createToolTip({title,text}) {
@@ -37,7 +38,7 @@ function createToolTip({title,text}) {
 function BuildSettings({group_name="mangadex-userscript-settings"}={}) {
   let settings = this;
   if (!( settings instanceof BuildSettings) ) {
-      return new BuildSettings(arguments);
+      return new BuildSettings(...arguments);
   }
   settings.group_name=group_name;
   // Get Mangadex Quick Setting's Container
@@ -49,129 +50,155 @@ function BuildSettings({group_name="mangadex-userscript-settings"}={}) {
   settings.dialog = xp.new('//div[@id="homepage_settings_modal"]/div').with(xp.new().contains('@class','modal-dialog')).getElement();
   settings.mangadex_body = xp.new('.//div').with(xp.new().contains('@class','modal-body')).getElement(settings.dialog);
 
-  settings.buildOptionItem = ({
+  function OptionItem({
     icon,
-    text,
+    title,
+    title_text,
     value,
     onselect = () => {return null;},
-    ontoggle = () => {return null;},
+    onchange = () => {return null;},
     ondeselect = () => {return null;},
     selected=false
-  }) => {
-    let item={};
-    item.ui = htmlToElement(`
+  }) {
+    let item = this;
+    if (!( item instanceof OptionItem) ) {
+        return new OptionItem(...arguments);
+    }
+    let ui = htmlToElement(`
       <li class="${selected ? "selected" : ""}">
       ${icon ? `<img class="" src="${icon}"/>` : "" }
-      <span class="">${text}</span>
+      <span class="">${title}</span>
       </li>
     `);
-    item.option=htmlToElement(`
-      <option  ${selected ? "selected" : "" } value="${value}"/>${text}</option>
+    item.elm=htmlToElement(`
+      <option  ${selected ? "selected" : "" } value="${value}"/>${title}</option>
     `);
-    item.option.select_callback=onselect;
-    item.option.deselect_callback=ondeselect;
-    item.option.toggle_callback=ontoggle;
-    item.option.dataset.contents = item.ui.innerHTML;
+    item.selected=selected;
+    item.elm.dataset.contents = ui.innerHTML;
+    item.elm.select_callback   = (new_value,old_value) => {
+      onselect(item,new_value,old_value);
+    };
+    item.elm.deselect_callback = (new_value,old_value) => {
+      ondeselect(item,new_value,old_value);
+    };
+    item.elm.change_callback = (new_value,old_value) => {
+      item.selected=new_value;
+      onchange(item,new_value,old_value);
+    };
 
     return item;
   };
-  settings.buildOptionItemManual = ({
-    icon,
-    text,
-    value,
-    onselect = () => {return null;},
-    ontoggle = () => {return null;},
-    ondeselect = () => {return null;},
-    selected=false
-  }) => {
-    let item={};
-    item.ui = htmlToElement(`
-      <li class="${selected ? "selected" : ""}">
-      ${icon ? `<img class="" src="${icon}"/>` : "" }
-      <span class="">${text}</span>
-      </li>
-    `);
-    item.option=htmlToElement(`
-      <option ${selected ? "selected" : "" } value="${value}"/>
-    `);
-    item.option.dataset.contents = item.ui.innerHTML;
-    item.selected_ui = item.ui.cloneNode();
-    item.button_ui_container = document.createElement("div");
-    item.ui.addEventListener("click",() => {
-      item.option.selected = !item.option.selected;
-      if (item.option.selected) {
-        item.ui.classList.add("selected");
-        item.button_ui_container.appendChild(item.selected_ui);
-        onselect(item);
-      }
-      else {
-        item.ui.classList.remove("selected");
-        item.button_ui_container.removeChild(item.selected_ui);
-        ondeselect(item);
-      }
-      ontoggle(item);
-    });
-    return item;
-  };
-  settings.buildMultiselect = ({setting_title}) => {
-    let setting={};
-    setting.container = htmlToElement(`<div class="form-group row">
-			<label class="col-lg-3 col-form-label-modal">${setting_title}:</label>
+
+  function Select({
+    title,
+    title_text,
+    multiselect=false,
+    onchange = () => {return null;},
+    options=[],
+  }) {
+    let setting = this;
+    if (!( setting instanceof Select) ) {
+        return new Select(...arguments);
+    }
+    let container = settings.mangadex_body;
+    setting.elm = htmlToElement(`<div class="form-group row">
+			<label class="col-lg-3 col-form-label-modal">${title}:</label>
 			<div class="col-lg-9">
-          <select multiple class="form-control selectpicker show-tick" data-actions-box="true" data-selected-text-format="count > 5" data-size="10" title="${setting_title}">
+          <select ${ multiselect ? "multiple" : ""} class="form-control selectpicker show-tick" data-actions-box="true" data-selected-text-format="count > 5" data-size="10" title="${title}">
           </select>
       </div>
     </div>`);
-    setting.label=setting.container.children[0];
-    setting.select=setting.container.children[1].children[0];
+    setting.label=setting.elm.children[0];
+    setting.select=setting.elm.children[1].children[0];
     let id=createSettingID();
     setting.id=id;
     setting.select.id=id;
-    settings.mangadex_body.appendChild(setting.container);
+    container.appendChild(setting.elm);
     $('#' + id ).on("changed.bs.select",function (e,clickedIndex,newValue,oldValue) {
         // New value is bool. oldValue is... array with random crap... for some reason....
-        setting.select.children[clickedIndex].toggle_callback(newValue,oldValue);
+        setting.select.children[clickedIndex].change_callback(newValue,oldValue);
         if (newValue) {
           setting.select.children[clickedIndex].select_callback(newValue,oldValue);
         }
         else {
           setting.select.children[clickedIndex].deselect_callback(newValue,oldValue);
         }
+        onchange(e,setting,clickedIndex,newValue,oldValue);
     });
+    setting.options={};
+    let last_used_value = -1;
+    function nextOptionValueToUse() {
+      return ++last_used_value;
+    }
+    setting.addExistingOption = (option) => {
+      if (setting.options[option.elm.value] != null) {
+        dbg("WARNING! Option value reused within Select! Removing original!");
+        setting.select.removeChild(setting.options[option.elm.value]);
+      }
+      setting.options[option.elm.value] = option;
+      setting.select.appendChild(option.elm);
+      last_used_value=option.value;
+    };
+    setting.addOption = (args) => {
+      setting.addExistingOption(new OptionItem({value:nextOptionValueToUse(), ...args}));
+    };
+
+    for (let [idx,option] of options.entries()) {
+      setting.addExistingOption(option);
+    }
+    return setting;
+  };
+  let settingObjects=[];
+  function addSetting(setting) {
+    settingObjects.push(setting);
+  }
+  settings.addMultiselect = (args) => {
+    let setting = new Select({multiselect:true,...args});
+    addSetting(setting);
+    return setting;
+  };
+  settings.addSelect = (args) => {
+    let setting = new Select(args);
+    addSetting(setting);
     return setting;
   };
   return settings;
 }
-function main() {
+function example() {
   let m = new BuildSettings();
-  dbg('HEre');
-  let select = m.buildMultiselect({setting_title:"SomeCrap"});
-  dbg('HEre');
+  let select = m.addMultiselect({title:"SomeCrap"});
   for (let [i,o] of ["just", "a","few","values"].entries()) {
-    dbg('HEre');
-    let item = m.buildOptionItem({
-      text: o,
+    let item = select.addOption({
+      title: o,
       value:i,
-      ontoggle: (item) => {
+      ontoggle: (item,value) => {
         // Do something
+        dbg(item);
+        dbg(value);
         dbg("changed!");
       },
-      onselect: (item) => {
+      onselect: (item,value) => {
         // Do something
+        dbg(item);
+        dbg(value);
         dbg("NOW SELECTED!");
       },
-      ondeselect: (item) => {
+      ondeselect: (item,value) => {
         // Do something
+        dbg(item);
+        dbg(value);
         dbg("NOW NOT SELECTED!");
       }
     });
-    //select.list_elm.appendChild(item.ui);
-    //select.button_contents.appendChild(item.button_ui_container);
   }
+  let selectAuto = m.addMultiselect({title:"Autocomplete"});
+  selectAuto.addOption({title:"Manga"});
+  selectAuto.addOption({title:"Users"});
 }
-
+/*
 let xp = new XPath();
 checkLoop({
   xpath:'//div[@id="homepage_settings_modal"]/div',
-  callback:main,
+  callback:example,
 });
+*/
