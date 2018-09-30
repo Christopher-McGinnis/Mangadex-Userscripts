9c7ea0f1641cb3e27e52d67c2de35790dd0310ca
FireFish5000 <1813610+firefish5000@users.noreply.github.com>
2018-09-30 00:24:51 -0500

    Move and rename functions others shouldn't use

--- uncommon.js
+++ uncommon.js
@@ -5,6 +5,40 @@
 // ==/UserScript==
 "use strict";
 
+// Dont forget to also memorize the keycodes for emoji! We have the full UTF-8 spectrum to cover....
+// But for now, lets memorize the word keycodes, end just try typing the name of the keys we want.
+const keycodes={
+  backspace:8,    tab:9,         enter:13,
+  shift:16,       ctrl:17,       alt:18,
+  pause_break:19, capslock:20,   escape:27,
+  space:32,       pageup:33,     pagedown:34,
+  end:35,         home:36,       leftarrow:37,
+  uparrow:38,     rightarrow:39, downarrow:40,
+  insert:45,      delete:46,
+  0:48,   1:49,   2:50,   3:51,
+  4:52,   5:53,   6:54,   7:55,
+  8:56,   9:57,   a:65,   b:66,
+  c:67,   d:68,   e:69,   f:70,
+  g:71,   h:72,   i:73,   j:74,
+  k:75,   l:76,   m:77,   n:78,
+  o:79,   p:80,   q:81,   r:82,
+  s:83,   t:84,   u:85,   v:86,
+  w:87,   x:88,   y:89,   z:90,
+  multiply: 106, add: 107, subtract: 109,
+  decimalpoint: 110, divide: 111,
+  f1: 112, f2: 113, f3: 114,
+  f4: 115, f5: 116, f6: 117,
+  f7: 118, f8: 119, f9: 120,
+  f10: 121, f11: 122, f12: 123,
+  numlock: 144, scrolllock: 145,
+  semicolon: 186, equalsign: 187,
+  comma: 188, dash: 189, period: 190,
+  forwardslash: 191, graveaccent: 192,
+  openbracket: 219, backslash: 220,
+  closebraket: 221, singlequote: 222
+};
+
+
 function dbg(x) {
   //unsafeWindow used soly for debugging in firefox via Web Console.
   if (typeof unsafeWindow === 'object') {
@@ -20,7 +54,7 @@ function dbg(x) {
  */
 
 // NOTE: I do not promot the use of this xpath builder. It is used soly to make ways I commonly use xpaths easier.
-// syntax will change. Do NOT depend on this. Do NOT use this. Do not think this is a good idea.
+// syntax will change. Do NOT depend on this. Do NOT use this. Do NOT think this is a good idea.
 // TODO: small AST?
 function XPath(xpath_str="") {
   let xp = this;
@@ -91,7 +125,7 @@ function XPath(xpath_str="") {
 
 
 /****************************************************************************
- *  Potentialy Usefull functions. Wating till follows standard, or stablized.
+ *  Potentialy Usefull functions. Waiting till follows standard, or stablized.
  */
 
 function throwMissingParam(name,param,example) {
@@ -111,165 +145,96 @@ function throwOnBadArg(condition,name,arg_name,example,bad_value) {
   }
 }
 
+
 /**
 A promise I promise you dont have to write.
-@param {Function} callback - Function we will call until true
-@param {Intager} [tries=50] - How many times we should try callback before giving up.
-@param {Intager} [delay=100] - How long we should wait between callback calls.
+@param {Function} callback - Function we will call until passes filter
+@param {Function} filter - Function we give the return value of callback to. should return true if we pass, false otherwise.
+@param {Intager} tries - How many times we should try callback before giving up.
+@param {Intager} delay - How long we should wait between callback calls.
 @param {String} [name] - Name to use for debugging
-@returns {Promise} Resolves when callback returns a true value. Rejects when runs out of tries.
+@returns {Promise} Resolves when callback's return value passes the filter. Rejects when runs out of tries.
 */
-
-function callUntilTrue({
-  callback = throwMissingParam('callUntilTrue','xpath','"String"'),
+function callUntilAccepted({
+  callback = throwMissingParam('callUntilAccepted','callback','Function'),
+  filter = throwMissingParam('callUntilAccepted','filter','(ret) => { return ret === true; }'),
+  tries = throwMissingParam('callUntilAccepted','tries','20'),
+  delay=throwMissingParam('callUntilAccepted','delay','1000'),
   name,
-  tries=50,
-  delay=100
 }) {
   return new Promise((resolve,reject) => {
-    function checkLoop(tries_left) {
+    function checkLoop(tries) {
+      let tries_left = tries - 1;
       new Promise((resolve,reject) => {
         let res = callback();
-        if (res) {
+        if (filter(res)) {
+          if (name) {
+            dbg(`<${name}> passed the filter with ${tries_left} tries remaining!`);
+          }
           resolve(res);
         }
-        reject(tries_left - 1);
+        reject(tries_left);
       }).then(resolve).catch((tries_left) => {
         if (tries_left > 0) {
           setTimeout(() => {checkLoop( tries_left );},delay);
         }
         else {
           if (name) {
-            dbg(`<${name}> failed to return a true value ${tries} times in ${delay}ms intervals. Giving up!`);
+            dbg(`<${name}> failed to pass the filter ${tries} times in ${delay}ms intervals. Giving up!`);
           }
           reject();
         }
       });
     }
     if (name) {
-      dbg(`Now waiting for <${name}> to return a true value!`);
+      dbg(`Now waiting for <${name}> to pass the filter!`);
     }
     checkLoop(tries);
   });
 }
-
-
+/**
+A promise I promise you dont have to write.
+@param {Function} callback - Function we will call until true
+@param {Intager} tries - How many times we should try callback before giving up.
+@param {Intager} delay - How long we should wait between callback calls.
+@param {String} [name] - Name to use for debugging
+@returns {Promise} Resolves when callback returns a true value. Rejects when runs out of tries.
+*/
+function callUntilTrue(args) {
+  return callUntilAccepted({...args, filter: (ret) => { return ret === true; } });
+}
+/**
+A promise I promise you dont have to write.
+@param {Function} callback - Function we will call until true
+@param {Intager} tries - How many times we should try callback before giving up.
+@param {Intager} delay - How long we should wait between callback calls.
+@param {String} [name] - Name to use for debugging
+@returns {Promise} Resolves when callback returns a defined value. Rejects when runs out of tries.
+*/
+function callUntilDefined(args) {
+  return callUntilAccepted({...args, filter: (ret) => { return (ret !== null && ret !== undefined); } });
+}
 // Checks the page for {xpath} every {delay} milliseconds up to {tries} times. Runs {callback} once found.
 // Used to wait for required elements to load before running functions.
 // xpath: A String or XPath instance
 // callback: Function to run once an xpath match is found
+/**
+Checks the page for {xpath} every {delay} milliseconds up to {tries} times.
+@param {String} xpath - XPath we are trying to resolve
+@param {Intager} tries - How many times we should try to find the xpath element before giving up.
+@param {Intager} delay - How long we should wait between searches.
+@param {String} [name] - Name to use for debugging
+@returns {Promise} Resolves when xpath elemant is found. Rejects when runs out of tries.
+*/
 function waitForElementByXpath({
   xpath = throwMissingParam('checkLoop','xpath','"String"'),
   tries=50,
   delay=100
 }) {
-  return new Promise((resolve,reject) => {
-    dbg(`Now waiting for xpath <${xpath}>`);
-    callUntilTrue({
-      callback:() => { return getElementByXpath(xpath); },
-      tries:tries,
-      name:"xpath resolver",
-      delay:delay,
-    }).then(resolve).catch(()=> { dbg(`Failed to find xpath <${xpath}> ${tries} times in ${delay} intervals. Giving up!`); });
+  return callUntilDefined({
+    callback:() => { return getElementByXpath(xpath); },
+    tries:tries,
+    name:`xpath <${xpath}>`,
+    delay:delay,
   });
-}
-// Gets all values for provided keys via GM_getValue, defaulting to the provided default values.
-// keys = {SomeGM_Key: SomeDefaultValue, AnotherGM_Key: AnotherDefaultValue}
-// fn: function toRunAfterAllGM_getValues_prommisesHaveFinished({
-//   SomeGM_Key: SomeValue,
-//   AnotherGM_Key: AnotherValue
-//})
-function getUserValue(key,defaultValue) {
-  return new Promise((resolve,reject) => {
-    let jsonDefault = JSON.stringify(defaultValue);
-    if (typeof GM === "object" && typeof GM.getValue === 'function') {
-      GM.getValue(key,jsonDefault).then((value) => {
-        resolve(JSON.parse(value));
-      });
-    }
-    else if (typeof GM_getValue === 'function') {
-        resolve(JSON.parse(GM_getValue(key,jsonDefault)));
-    }
-    else {
-      reject("To use 'getUserValue' you must grant either GM.getValue or GM_getValue.");
-    }
-  });
-}
-function getUserValues(keys) {
-  let prommises=[];
-  Object.entries(keys).forEach(([key,defaultValue]) => {
-    prommises.push(getUserValue(key,defaultValue));
-  });
-  return Promise.all(prommises);
-}
-
-function setUserValue(key,value) {
-  return new Promise((resolve,reject) => {
-    if (typeof GM === "object" && typeof GM.setValue === 'function') {
-      GM.setValue(key,JSON.stringify(value)).then(resolve).catch(reject);
-    }
-    else if (typeof GM_setValue === 'function') {
-      GM_setValue(key,JSON.stringify(value));
-      resolve();
-    }
-    else {
-      reject("To use 'setUserValue' you must grant either GM.setValue or GM_setValue.");
-    }
-  });
-}
-function setUserValues(objs) {
-  let prommises=[];
-  Object.entries(objs).forEach(([key,value]) => {
-    prommises.push(setUserValue(key,value));
-  });
-  return Promise.all(prommises);
-}
-
-const keycodes={
-  backspace:8,    tab:9,         enter:13,
-  shift:16,       ctrl:17,       alt:18,
-  pause_break:19, capslock:20,   escape:27,
-  space:32,       pageup:33,     pagedown:34,
-  end:35,         home:36,       leftarrow:37,
-  uparrow:38,     rightarrow:39, downarrow:40,
-  insert:45,      delete:46,
-  0:48,   1:49,   2:50,   3:51,
-  4:52,   5:53,   6:54,   7:55,
-  8:56,   9:57,   a:65,   b:66,
-  c:67,   d:68,   e:69,   f:70,
-  g:71,   h:72,   i:73,   j:74,
-  k:75,   l:76,   m:77,   n:78,
-  o:79,   p:80,   q:81,   r:82,
-  s:83,   t:84,   u:85,   v:86,
-  w:87,   x:88,   y:89,   z:90,
-  multiply: 106, add: 107, subtract: 109,
-  decimalpoint: 110, divide: 111,
-  f1: 112, f2: 113, f3: 114,
-  f4: 115, f5: 116, f6: 117,
-  f7: 118, f8: 119, f9: 120,
-  f10: 121, f11: 122, f12: 123,
-  numlock: 144, scrolllock: 145,
-  semicolon: 186, equalsign: 187,
-  comma: 188, dash: 189, period: 190,
-  forwardslash: 191, graveaccent: 192,
-  openbracket: 219, backslash: 220,
-  closebraket: 221, singlequote: 222
-};
-
-function createToolTip({title,text}) {
-  let tooltip_elm = htmlToElement(`<div>${title}<br><span>${text}</span></div>`);
-  let tooltip_text = tooltip_elm.children[1];
-  tooltip_elm.style.display="none";
-  tooltip_elm.style.backgroundColor="rgba(15,15,15,0.9)";
-  tooltip_elm.style.borderRadius="15px";
-  tooltip_elm.style.color="rgb(215,215,215)";
-  tooltip_elm.style.left="0%";
-  tooltip_elm.style.position="absolute";
-  tooltip_elm.style.zIndex=10;
-  tooltip_elm.style.textAlign="center";
-  document.body.appendChild(tooltip_elm);
-  return {
-    tooltip:tooltip_elm,
-    text_container:tooltip_text
-  };
 }
