15510350de16f4d20b5d7664ee3b66415995c109
Brandon R. Beck <brandonrbeck101@gmail.com>
2018-10-04 03:34:23 -0500

    Linter: pass 1 libraries
    automatic linting.
    
    seems to have worked.

--- common.js
+++ common.js
@@ -3,85 +3,88 @@
 // @version  0.0.5
 // @description Common function library for Mangadex. Should be required by other userscripts.
 // ==/UserScript==
-"use strict";
+/* eslint no-unused-vars: ["off"] */
+
+'use strict'
 
 function dbg(x) {
-  //unsafeWindow used soly for debugging in firefox via Web Console.
+  // unsafeWindow used soly for debugging in firefox via Web Console.
   if (typeof unsafeWindow === 'object') {
-    unsafeWindow.console.log(x);
+    unsafeWindow.console.log(x)
   }
   else {
-    console.log(x);
+    console.log(x)
   }
 }
 function htmlToElement(html) {
-  var template = document.createElement('template');
-  html = html.trim(); // Never return a text node of whitespace as the result
-  template.innerHTML = html;
-  return template.content.firstChild;
+  const template = document.createElement('template')
+  html = html.trim() // Never return a text node of whitespace as the result
+  template.innerHTML = html
+  return template.content.firstChild
 }
 function fallbackCopyTextToClipboard(text) {
-  var textArea = document.createElement("textarea");
-  textArea.style.position="fixed";
-  textArea.style.top="50%";
-  textArea.style.left="50%";
-  textArea.style.marginTop="-10px";
-  textArea.style.marginLeft="-10px";
-  textArea.style.width="20px";
-  textArea.style.height="20px";
-  textArea.style.opacity="0";
-  textArea.value = text;
-  document.body.appendChild(textArea);
-  textArea.focus();
-  textArea.select();
+  const textArea = document.createElement('textarea')
+  textArea.style.position = 'fixed'
+  textArea.style.top = '50%'
+  textArea.style.left = '50%'
+  textArea.style.marginTop = '-10px'
+  textArea.style.marginLeft = '-10px'
+  textArea.style.width = '20px'
+  textArea.style.height = '20px'
+  textArea.style.opacity = '0'
+  textArea.value = text
+  document.body.appendChild(textArea)
+  textArea.focus()
+  textArea.select()
   try {
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Fallback: Copying text command was ' + msg);
-  } catch (err) {
-    console.error('Fallback: Oops, unable to copy', err);
+    const successful = document.execCommand('copy')
+    const msg = successful ? 'successful' : 'unsuccessful'
+    console.log(`Fallback: Copying text command was ${msg}`)
   }
-  document.body.removeChild(textArea);
+  catch (err) {
+    console.error('Fallback: Oops, unable to copy', err)
+  }
+  document.body.removeChild(textArea)
 }
 function copyTextToClipboard(text) {
   // First try to copy using the 2 GM methods..
-  if (typeof GM === "object" && typeof GM.setClipboard === 'function') {
-    GM.setClipboard(text);
+  if (typeof GM === 'object' && typeof GM.setClipboard === 'function') {
+    GM.setClipboard(text)
   }
   else if (typeof GM_setClipboard === 'function') {
-    GM_setClipboard(text);
+    GM_setClipboard(text)
   }
   // Programmer failed to grant setClipboard permissions.
   // Attempt to use browser supported methods.
   else if (navigator && navigator.clipboard) {
-    navigator.clipboard.writeText(text).then(function() {
-      dbg('Async: Copying to clipboard was successful!');
-    }, function(err) {
-      dbg('Async: Could not copy text: ', err);
-    });
+    navigator.clipboard.writeText(text).then(() => {
+      dbg('Async: Copying to clipboard was successful!')
+    }, (err) => {
+      dbg('Async: Could not copy text: ', err)
+    })
   }
   else {
-    fallbackCopyTextToClipboard(text);
+    fallbackCopyTextToClipboard(text)
   }
 }
-/**************************************************
+/** ************************************************
  * XPath
  */
 
-function getSnapshotByXpath(path,node=document) {
-  return document.evaluate( path.toString() , node, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+function getSnapshotByXpath(path, node = document) {
+  return document.evaluate(path.toString(), node, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null)
 }
-function getOrderedSnapshotByXpath(path,node=document) {
-  return document.evaluate( path.toString() , node, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+function getOrderedSnapshotByXpath(path, node = document) {
+  return document.evaluate(path.toString(), node, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null)
 }
-function getItterByXpath(path,node=document) {
-  return document.evaluate( path.toString(), node, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+function getItterByXpath(path, node = document) {
+  return document.evaluate(path.toString(), node, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null)
 }
-function getOrderedItterByXpath(path,node=document) {
-  return document.evaluate( path.toString(), node, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+function getOrderedItterByXpath(path, node = document) {
+  return document.evaluate(path.toString(), node, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null)
 }
-function getElementByXpath(path,node=document) {
-  return document.evaluate( path.toString(), node, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+function getElementByXpath(path, node = document) {
+  return document.evaluate(path.toString(), node, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
 }
 
 // Gets all values for provided keys via GM_getValue, defaulting to the provided default values.
@@ -89,75 +92,77 @@ function getElementByXpath(path,node=document) {
 // fn: function toRunAfterAllGM_getValues_prommisesHaveFinished({
 //   SomeGM_Key: SomeValue,
 //   AnotherGM_Key: AnotherValue
-//})
-function getUserValue(key,defaultValue) {
-  return new Promise((resolve,reject) => {
-    let jsonDefault = JSON.stringify(defaultValue);
-    if (typeof GM === "object" && typeof GM.getValue === 'function') {
-      GM.getValue(key,jsonDefault).then((value) => {
-        resolve(JSON.parse(value));
-      });
+// })
+function getUserValue(key, defaultValue) {
+  return new Promise((resolve, reject) => {
+    const jsonDefault = JSON.stringify(defaultValue)
+    if (typeof GM === 'object' && typeof GM.getValue === 'function') {
+      GM.getValue(key, jsonDefault).then((value) => {
+        resolve(JSON.parse(value))
+      })
     }
     else if (typeof GM_getValue === 'function') {
-      resolve(JSON.parse(GM_getValue(key,jsonDefault)));
+      resolve(JSON.parse(GM_getValue(key, jsonDefault)))
     }
     else {
-      reject("To use 'getUserValue' you must grant either GM.getValue or GM_getValue.");
+      reject(new Error("To use 'getUserValue' you must grant either GM.getValue or GM_getValue."))
     }
-  });
+  })
 }
 function getUserValues(keys) {
-  let prommises=[];
-  Object.entries(keys).forEach(([key,defaultValue]) => {
+  const prommises = []
+  Object.entries(keys).forEach(([key, defaultValue]) => {
     prommises.push(
-       getUserValue(key,defaultValue).then((v) => { let obj={}; obj[key]=v; return obj; } )
-    );
-  });
-  return Promise.all(prommises).then( (itter) => {
-    let new_obj={};
-    for (let obj of itter) {
-      Object.assign(new_obj,obj);
+      getUserValue(key, defaultValue).then((v) => {
+        const obj = {}; obj[key] = v; return obj
+      }),
+    )
+  })
+  return Promise.all(prommises).then((itter) => {
+    const new_obj = {}
+    for (const obj of itter) {
+      Object.assign(new_obj, obj)
     }
-    return new_obj;
-  });
+    return new_obj
+  })
 }
 
-function setUserValue(key,value) {
-  return new Promise((resolve,reject) => {
-    if (typeof GM === "object" && typeof GM.setValue === 'function') {
-      GM.setValue(key,JSON.stringify(value)).then(resolve).catch(reject);
+function setUserValue(key, value) {
+  return new Promise((resolve, reject) => {
+    if (typeof GM === 'object' && typeof GM.setValue === 'function') {
+      GM.setValue(key, JSON.stringify(value)).then(resolve).catch(reject)
     }
     else if (typeof GM_setValue === 'function') {
-      GM_setValue(key,JSON.stringify(value));
-      resolve();
+      GM_setValue(key, JSON.stringify(value))
+      resolve()
     }
     else {
-      reject("To use 'setUserValue' you must grant either GM.setValue or GM_setValue.");
+      reject(Error("To use 'setUserValue' you must grant either GM.setValue or GM_setValue."))
     }
-  });
+  })
 }
 function setUserValues(objs) {
-  let prommises=[];
-  Object.entries(objs).forEach(([key,value]) => {
-    prommises.push(setUserValue(key,value));
-  });
-  return Promise.all(prommises);
+  const prommises = []
+  Object.entries(objs).forEach(([key, value]) => {
+    prommises.push(setUserValue(key, value))
+  })
+  return Promise.all(prommises)
 }
 
-function createToolTip({title,text}) {
-  let tooltip_elm = htmlToElement(`<div>${title}<br><span>${text}</span></div>`);
-  let tooltip_text = tooltip_elm.children[1];
-  tooltip_elm.style.display="none";
-  tooltip_elm.style.backgroundColor="rgba(15,15,15,0.9)";
-  tooltip_elm.style.borderRadius="15px";
-  tooltip_elm.style.color="rgb(215,215,215)";
-  tooltip_elm.style.left="0%";
-  tooltip_elm.style.position="absolute";
-  tooltip_elm.style.zIndex=10;
-  tooltip_elm.style.textAlign="center";
-  document.body.appendChild(tooltip_elm);
+function createToolTip({ title, text }) {
+  const tooltip_elm = htmlToElement(`<div>${title}<br><span>${text}</span></div>`)
+  const tooltip_text = tooltip_elm.children[1]
+  tooltip_elm.style.display = 'none'
+  tooltip_elm.style.backgroundColor = 'rgba(15,15,15,0.9)'
+  tooltip_elm.style.borderRadius = '15px'
+  tooltip_elm.style.color = 'rgb(215,215,215)'
+  tooltip_elm.style.left = '0%'
+  tooltip_elm.style.position = 'absolute'
+  tooltip_elm.style.zIndex = 10
+  tooltip_elm.style.textAlign = 'center'
+  document.body.appendChild(tooltip_elm)
   return {
-    tooltip:tooltip_elm,
-    text_container:tooltip_text
-  };
+    tooltip: tooltip_elm
+    , text_container: tooltip_text
+  }
 }
--- uncommon.js
+++ uncommon.js
@@ -3,145 +3,156 @@
 // @version  0.0.5
 // @description WARNING Should NOT be required by other userscripts. Lets be honest, no one else wants this crap. This is a personal library for personal problems.
 // ==/UserScript==
-"use strict";
+/* eslint no-unused-vars: ["off"] */
+
+'use strict'
 
 // Dont forget to also memorize the keycodes for emoji! We have the full UTF-8 spectrum to cover....
 // But for now, lets memorize the word keycodes, end just try typing the name of the keys we want.
-const keycodes={
-  backspace:8,    tab:9,         enter:13,
-  shift:16,       ctrl:17,       alt:18,
-  pause_break:19, capslock:20,   escape:27,
-  space:32,       pageup:33,     pagedown:34,
-  end:35,         home:36,       leftarrow:37,
-  uparrow:38,     rightarrow:39, downarrow:40,
-  insert:45,      delete:46,
-  0:48,   1:49,   2:50,   3:51,
-  4:52,   5:53,   6:54,   7:55,
-  8:56,   9:57,   a:65,   b:66,
-  c:67,   d:68,   e:69,   f:70,
-  g:71,   h:72,   i:73,   j:74,
-  k:75,   l:76,   m:77,   n:78,
-  o:79,   p:80,   q:81,   r:82,
-  s:83,   t:84,   u:85,   v:86,
-  w:87,   x:88,   y:89,   z:90,
-  multiply: 106, add: 107, subtract: 109,
-  decimalpoint: 110, divide: 111,
-  f1: 112, f2: 113, f3: 114,
-  f4: 115, f5: 116, f6: 117,
-  f7: 118, f8: 119, f9: 120,
-  f10: 121, f11: 122, f12: 123,
-  numlock: 144, scrolllock: 145,
-  semicolon: 186, equalsign: 187,
-  comma: 188, dash: 189, period: 190,
-  forwardslash: 191, graveaccent: 192,
-  openbracket: 219, backslash: 220,
-  closebraket: 221, singlequote: 222
-};
+/* eslint object-property-newline: ["off", { "allowAllPropertiesOnSameLine": true }] */
+/* eslint no-multi-spaces: ["off"] */
+const keycodes = {
+  backspace: 8,    tab: 9,         enter: 13
+  , shift: 16,       ctrl: 17,       alt: 18
+  , pause_break: 19, capslock: 20,   escape: 27
+  , space: 32,       pageup: 33,     pagedown: 34
+  , end: 35,         home: 36,       leftarrow: 37
+  , uparrow: 38,     rightarrow: 39, downarrow: 40
+  , insert: 45,      delete: 46
+  , 0: 48,   1: 49,   2: 50,   3: 51
+  , 4: 52,   5: 53,   6: 54,   7: 55
+  , 8: 56,   9: 57,   a: 65,   b: 66
+  , c: 67,   d: 68,   e: 69,   f: 70
+  , g: 71,   h: 72,   i: 73,   j: 74
+  , k: 75,   l: 76,   m: 77,   n: 78
+  , o: 79,   p: 80,   q: 81,   r: 82
+  , s: 83,   t: 84,   u: 85,   v: 86
+  , w: 87,   x: 88,   y: 89,   z: 90
+  , multiply: 106, add: 107, subtract: 109
+  , decimalpoint: 110, divide: 111
+  , f1: 112, f2: 113, f3: 114
+  , f4: 115, f5: 116, f6: 117
+  , f7: 118, f8: 119, f9: 120
+  , f10: 121, f11: 122, f12: 123
+  , numlock: 144, scrolllock: 145
+  , semicolon: 186, equalsign: 187
+  , comma: 188, dash: 189, period: 190
+  , forwardslash: 191, graveaccent: 192
+  , openbracket: 219, backslash: 220
+  , closebraket: 221, singlequote: 222
+}
 
 
 function dbg(x) {
-  //unsafeWindow used soly for debugging in firefox via Web Console.
+  // unsafeWindow used soly for debugging in firefox via Web Console.
   if (typeof unsafeWindow === 'object') {
-    unsafeWindow.console.log(x);
+    unsafeWindow.console.log(x)
   }
   else {
-    console.log(x);
+    console.log(x)
   }
 }
 
-/**************************************************
+/** ************************************************
  * XPath
  */
 
 // NOTE: I do not promot the use of this xpath builder. It is used soly to make ways I commonly use xpaths easier.
 // syntax will change. Do NOT depend on this. Do NOT use this. Do NOT think this is a good idea.
 // TODO: small AST?
-function XPath(xpath_str="") {
-  let xp = this;
-  if (!( xp instanceof XPath) ) {
-	    return new XPath(xpath_str);
-	}
-  xp.xpath=xpath_str;
+function XPath(xpath_str = '') {
+  const xp = this
+  if (!(xp instanceof XPath)) {
+	    return new XPath(xpath_str)
+  }
+  xp.xpath = xpath_str
   function toStr(o) {
     if (o instanceof XPath) {
-      return o.toString();
+      return o.toString()
     }
-    else {
-      return o;
+
+    return o
+  }
+  xp.new = function (xpath_str) {
+    return new XPath(xpath_str)
+  }
+  xp.clone = function () {
+    return new XPath(xp.xpath)
+  }
+  xp.contains = function (attr, text = throwMissingArg('XPath().contains(attr,text)', 'text', '("@class","String")')) {
+    xp.xpath += `contains(concat(' ', normalize-space(${attr}), ' '), ' ${text} ')`
+    return xp
+  }
+  xp.with = function (selector) {
+    xp.xpath += `[${toStr(selector)}]`
+    return xp
+  }
+  xp.append = function (text) {
+    xp.xpath += text
+    return xp
+  }
+  xp.and = function (o = '') {
+    xp.xpath += ` and ${toStr(o)}`
+    return xp
+  }
+  xp.or = function (o = '') {
+    xp.xpath += ` or ${toStr(o)}`
+    return xp
+  }
+  xp.toString = function () {
+    return xp.xpath
+  }
+  xp.getElement = function (node = document) {
+    return getElementByXpath(xp, node)
+  }
+  xp.getSnapshot = function (node = document) {
+    return getSnapshotByXpath(xp, node)
+  }
+  xp.getOrderedSnapshot = function (node = document) {
+    return getOrderedSnapshotByXpath(xp, node)
+  }
+  xp.getItter = function (node = document) {
+    return getItterByXpath(xp, node)
+  }
+  xp.getOrderedItter = function (node = document) {
+    return getOrderedItterByXpath(xp, node)
+  }
+  xp.forEachElement = (fn, node) => {
+    for (let [i, item] = [xp.getItter()]; (() => {
+      item = i.iterateNext(); return item
+    })();) {
+      fn(item)
     }
   }
-  xp.new=function(xpath_str) { return new XPath(xpath_str); };
-  xp.clone=function() { return new XPath(xp.xpath); };
-  xp.contains = function(attr,text=throwMissingArg('XPath().contains(attr,text)','text','("@class","String")')) {
-    xp.xpath += `contains(concat(' ', normalize-space(${attr}), ' '), ' ${text} ')`;
-    return xp;
-  };
-  xp.with = function(selector) {
-    xp.xpath += `[${toStr(selector)}]`;
-    return xp;
-  };
-  xp.append = function(text) {
-    xp.xpath += text;
-    return xp;
-  };
-  xp.and = function(o='') {
-    xp.xpath += " and " + toStr(o);
-    return xp;
+  xp.forEachOrderedElement = (fn, node) => {
+    for (let [i, item] = [xp.getOrderedItter()]; (() => {
+      item = i.iterateNext(); return item
+    })();) {
+      fn(item)
+    }
   }
-  xp.or = function(o='') {
-    xp.xpath += " or " + toStr(o);
-    return xp;
-  }
-  xp.toString = function() {
-    return xp.xpath;
-  };
-  xp.getElement = function(node=document) {
-    return getElementByXpath(xp, node);
-  };
-  xp.getSnapshot = function(node=document) {
-    return getSnapshotByXpath(xp, node);
-  };
-  xp.getOrderedSnapshot = function(node=document) {
-    return getOrderedSnapshotByXpath(xp, node);
-  };
-  xp.getItter = function(node=document) {
-    return getItterByXpath(xp, node);
-  };
-  xp.getOrderedItter = function(node=document) {
-    return getOrderedItterByXpath(xp, node);
-  };
-  xp.forEachElement = (fn,node) => {
-    for(let [i,item] = [xp.getItter()]; (()=>{item=i.iterateNext(); return item;})();) {
-      fn(item);
-    };
-  }
-  xp.forEachOrderedElement = (fn,node) => {
-    for(let [i,item] = [xp.getOrderedItter()]; (()=>{item=i.iterateNext(); return item;})();) {
-      fn(item);
-    };
-  }
-  return xp;
+  return xp
 }
 
 
-/****************************************************************************
+/** **************************************************************************
  *  Potentialy Usefull functions. Waiting till follows standard, or stablized.
  */
 
-function throwMissingParam(name,param,example) {
-    throw new Error(`Function <${name}> is missing required parameter: <${param}>${example ? ` eg. <${param}: ${example}>` : "" }`);
+function throwMissingParam(name, param, example) {
+  throw new Error(`Function <${name}> is missing required parameter: <${param}>${example ? ` eg. <${param}: ${example}>` : ''}`)
 }
-function throwMissingArg(name,arg_name,example) {
-    throw new Error(`Function <${name}> is missing required argument: <${arg_name}>${example ? ` eg. <${example}>` : "" }`);
+function throwMissingArg(name, arg_name, example) {
+  throw new Error(`Function <${name}> is missing required argument: <${arg_name}>${example ? ` eg. <${example}>` : ''}`)
 }
-function throwOnBadParam(condition,name,param,example,bad_value) {
+function throwOnBadParam(condition, name, param, example, bad_value) {
   if (condition) {
-    throw new Error(`Function <${name}> has illegal value for required parameter: <${param}>${example ? ` exected: <${example}>` : "" }${bad_value ? ` got: <${bad_value}>`  : "" }`);
+    throw new Error(`Function <${name}> has illegal value for required parameter: <${param}>${example ? ` exected: <${example}>` : ''}${bad_value ? ` got: <${bad_value}>`  : ''}`)
   }
 }
-function throwOnBadArg(condition,name,arg_name,example,bad_value) {
+function throwOnBadArg(condition, name, arg_name, example, bad_value) {
   if (condition) {
-    throw new Error(`Function <${name}> has illegal value for required argument: <${arg_name}>${example ? ` exected: <${example}>` : "" }${bad_value ? ` got: <${bad_value}>`  : "" }`);
+    throw new Error(`Function <${name}> has illegal value for required argument: <${arg_name}>${example ? ` exected: <${example}>` : ''}${bad_value ? ` got: <${bad_value}>`  : ''}`)
   }
 }
 
@@ -156,41 +167,43 @@ A promise I promise you dont have to write.
 @returns {Promise} Resolves when callback's return value passes the filter. Rejects when runs out of tries.
 */
 function callUntilAccepted({
-  callback = throwMissingParam('callUntilAccepted','callback','Function'),
-  filter = throwMissingParam('callUntilAccepted','filter','(ret) => { return ret === true; }'),
-  tries = throwMissingParam('callUntilAccepted','tries','20'),
-  delay=throwMissingParam('callUntilAccepted','delay','1000'),
-  name,
+  callback = throwMissingParam('callUntilAccepted', 'callback', 'Function'),
+  filter = throwMissingParam('callUntilAccepted', 'filter', '(ret) => { return ret === true; }'),
+  tries = throwMissingParam('callUntilAccepted', 'tries', '20'),
+  delay = throwMissingParam('callUntilAccepted', 'delay', '1000'),
+  name
 }) {
-  return new Promise((resolve,reject) => {
+  return new Promise((resolve, reject) => {
     function checkLoop(tries) {
-      let tries_left = tries - 1;
-      new Promise((resolve,reject) => {
-        let res = callback();
+      const tries_left = tries - 1
+      new Promise((resolve, reject) => {
+        const res = callback()
         if (filter(res)) {
           if (name) {
-            dbg(`<${name}> passed the filter with ${tries_left} tries remaining!`);
+            dbg(`<${name}> passed the filter with ${tries_left} tries remaining!`)
           }
-          resolve(res);
+          resolve(res)
         }
-        reject(tries_left);
+        reject(tries_left)
       }).then(resolve).catch((tries_left) => {
         if (tries_left > 0) {
-          setTimeout(() => {checkLoop( tries_left );},delay);
+          setTimeout(() => {
+            checkLoop(tries_left)
+          }, delay)
         }
         else {
           if (name) {
-            dbg(`<${name}> failed to pass the filter ${tries} times in ${delay}ms intervals. Giving up!`);
+            dbg(`<${name}> failed to pass the filter ${tries} times in ${delay}ms intervals. Giving up!`)
           }
-          reject();
+          reject()
         }
-      });
+      })
     }
     if (name) {
-      dbg(`Now waiting for <${name}> to pass the filter!`);
+      dbg(`Now waiting for <${name}> to pass the filter!`)
     }
-    checkLoop(tries);
-  });
+    checkLoop(tries)
+  })
 }
 /**
 A promise I promise you dont have to write.
@@ -201,7 +214,7 @@ A promise I promise you dont have to write.
 @returns {Promise} Resolves when callback returns a true value. Rejects when runs out of tries.
 */
 function callUntilTrue(args) {
-  return callUntilAccepted({...args, filter: (ret) => { return ret === true; } });
+  return callUntilAccepted({ ...args, filter: ret => ret === true })
 }
 /**
 A promise I promise you dont have to write.
@@ -212,7 +225,7 @@ A promise I promise you dont have to write.
 @returns {Promise} Resolves when callback returns a defined value. Rejects when runs out of tries.
 */
 function callUntilDefined(args) {
-  return callUntilAccepted({...args, filter: (ret) => { return (ret !== null && ret !== undefined); } });
+  return callUntilAccepted({ ...args, filter: ret => (ret !== null && ret !== undefined) })
 }
 // Checks the page for {xpath} every {delay} milliseconds up to {tries} times. Runs {callback} once found.
 // Used to wait for required elements to load before running functions.
@@ -227,14 +240,14 @@ Checks the page for {xpath} every {delay} milliseconds up to {tries} times.
 @returns {Promise} Resolves when xpath elemant is found. Rejects when runs out of tries.
 */
 function waitForElementByXpath({
-  xpath = throwMissingParam('checkLoop','xpath','"String"'),
-  tries=50,
-  delay=100
+  xpath = throwMissingParam('checkLoop', 'xpath', '"String"'),
+  tries = 50,
+  delay = 100
 }) {
   return callUntilDefined({
-    callback:() => { return getElementByXpath(xpath); },
-    tries:tries,
-    name:`xpath <${xpath}>`,
-    delay:delay,
-  });
+    callback: () => getElementByXpath(xpath)
+    , tries
+    , name: `xpath <${xpath}>`
+    , delay
+  })
 }
