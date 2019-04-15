73ea76236a2c56fd3c237890d7ab8a7c780f4249
FireFish5000 <1813610+firefish5000@users.noreply.github.com>
2019-04-14 19:56:08 -0500

    Use PEG grammer and add preview update delays
    Update delay automatically calculated based on how long it takes to update the preview.

--- preview-post.user.js
+++ preview-post.user.js
@@ -2,7 +2,7 @@
 // @name     Mangadex Preview Post
 // @description Preview new forum/comment posts and edits on MangaDex.
 // @namespace https://github.com/Christopher-McGinnis
-// @version  0.0.5
+// @version  0.0.6
 // @grant    unsafeWindow
 // @grant    GM.getValue
 // @grant    GM.setValue
@@ -11,13 +11,15 @@
 // @require  https://gitcdn.xyz/repo/Christopher-McGinnis/Mangadex-Userscripts/a480c30b64fba63fad4e161cdae01e093bce1e4c/common.js
 // @require  https://gitcdn.xyz/repo/Christopher-McGinnis/Mangadex-Userscripts/21ec54406809722c425c39a0f5b6aad59fb3d88d/uncommon.js
 // @require  https://gitcdn.xyz/repo/Christopher-McGinnis/Mangadex-Userscripts/0d46bb0b3fa43f11ea904945e7baef7c6e2a6a5b/settings-ui.js
+// @require  https://pegjs.org/vendor/pegjs/peg.js
 // @match    https://mangadex.org/*
 // @author   Christopher McGinnis
 // @icon     https://mangadex.org/images/misc/default_brand.png
 // @license  MIT
 // ==/UserScript==
-// 
-// 
+// @require  https://gitcdn.xyz/repo/pegjs/pegjs/master/website/vendor/pegjs/peg.js
+
+
 
 
 
@@ -98,6 +100,7 @@ const bbCodeParser = new BBCode({
     '\\[i\\](.*?)\\[/i\\]': '<em>$1</em>',
     '\\[u\\](.*?)\\[/u\\]': '<u>$1</u>',
     '\\[s\\](.*?)\\[/s\\]': '<s>$1</s>',
+    '\\[code\\](.*?)\\[/code\\]': '<code>$1</code>',
 
     '\\[h1\\](.*?)\\[/h1\\]': '<h1>$1</h1>',
     '\\[h2\\](.*?)\\[/h2\\]': '<h2>$1</h2>',
@@ -128,16 +131,180 @@ const bbCodeParser = new BBCode({
 });
 
 // define configuration function for default
-bbCodeParser.create = BBCode;
+bbCodeParser.create = BBCode
+
+
+/* PEG grammer */
+let bbcodePegParser = peg.generate(String.raw`
+start = res:Expression? {return res}
+Expression = res:(Tag / LineBreak / Text )+ {return res}
+/*head:Term tail:(_ ("+" / "-") _ Term)* {
+      return tail.reduce(function(result, element) {
+        if (element[1] === "+") { return result + element[3]; }
+        if (element[1] === "-") { return result - element[3]; }
+      }, head);
+    }
+*/
+Tag = tag:(OpenCloseTag / PrefixTag) {return tag}
+OpenCloseTag = open:(OpenTag / OpenDataTag) content:Expression? close:CloseTag?
+  &{
+    let hasClose = close != null
+    if (false && hasClose && open.tag != close.tag) {
+      throw new Error(
+          "Expected [/" + open.tag + "] but [/" + close.tag + "] found."
+      );
+	}
+    return true
+} {
+    return {type:open.tag, data:open.attr, content}
+}
+PrefixTag = "[" tag:PrefixTagList "]" { return {type:tag} }
+
+// PrefixTag = "[" tag:PrefixTagList "]" content:(!("[/" ListTags "]" / LineBreak ) .)* { return {type:tag,unparsed:content.join('')} }
+
+ListTags = "list" / "ul" / "ol" / "li"
+
+NormalTagList = "list" / "spoiler" / "center" / "code" / "img" /  "sub" / "sup" / "left" / "right" / "ol" / "ul" / "h1" / "h2" / "h3" / "h4" / "h5" / "h6" / "hr" / "b" / "s" / "i" / "u"
+DataTagList = "url" 
+PrefixTagList = "*"
+
+Data
+  = text:(!"]". Data?) {
+  /*if(text[2] != null) {
+    return {type: "data", content:text[1] + text[2].content }
+  }
+  return {type: "data", content:text[1] }
+  */
+  if(text[2] != null) {
+    return text[1] + text[2]
+  }
+  return text[1]
+}
+OpenTag = "[" tag:NormalTagList "]" { return {tag} }
+AttrTagProxy = "=" attr:Data? {return attr}
+OpenDataTag = "[" tag:DataTagList attr:AttrTagProxy?  "]" { return {tag,attr:attr} }
+
+
+CloseTag = "[/" tag:(DataTagList / NormalTagList ) "]" { return {tag} }
+
+
+Text
+  = text:(!(Tag / CloseTag / LineBreak). Text?) {
+  if(text[2] != null) {
+    return {type: "text", content:text[1] + text[2].content }
+  }
+  return {type: "text", content:text[1] }
+}
+LineBreak
+  = [\n] {
+  return {type: "linebreak" }
+}
+ErrorCatcher
+  = errTxt:. {return {type: "error", content: errTxt} }
+
+_ "whitespace"
+  = [ \t\n\r]*
+`)
+
+
+
+
 
 
 /* main code */
 
 
-
+function pegAstToHtml(ast) {
+  let in_li = false
+  if (ast == null) {
+    return ""
+  }
+  if (typeof(ast) !== "object") {
+    return ast
+  }
+  //dbg(ast)
+  //Object.values(ast)
+  let res =  ast.reduce((accum, e) => {
+    function try_exit_li() {
+      if (in_li) {
+        in_li=false
+        accum += "</li>"
+        return true
+      }
+      return false
+    }
+    if (e.type == "text") {
+      accum += e.content
+    }
+    else if (e.type == "linebreak") {
+      if (! try_exit_li() ) {
+        accum += "<br>"
+      }
+    }
+    else if (e.type.match(/^(u|s|sub|sup|ol|code)$/)) {
+      accum += `<${e.type}>${pegAstToHtml(e.content)}</${e.type}>`
+    }
+    else if (e.type.match(/^(list|ul)$/)) {
+      accum += `<ul>${pegAstToHtml(e.content)}</ul>`
+    }
+    else if (e.type.match(/^h[123456]$/)) {
+      accum += `<${e.type}>${pegAstToHtml(e.content)}</${e.type}>`
+    }
+    else if (e.type.match(/^hr$/)) {
+      accum += `<${e.type}>${pegAstToHtml(e.content)}`
+    }
+    else if (e.type.match(/^b$/)) {
+      accum += `<strong>${pegAstToHtml(e.content)}</strong>`
+    }
+    else if (e.type.match(/^i$/)) {
+      accum += `<em>${pegAstToHtml(e.content)}</em>`
+    }
+    else if (e.type.match(/^url$/)) {
+      accum += `<a href="${e.data}" target="_blank">${pegAstToHtml(e.content)}</a>`
+    }
+    else if (e.type.match(/^img$/)) {
+      accum += `<img src="${pegAstToHtml(e.content)}"/>`
+    }
+    else if (e.type.match(/^quote$/)) {
+      accum += `<div style="width: 100%; display: inline-block; margin: 1em 0;" class="well well-sm">${pegAstToHtml(e.content)}</div>`
+    }
+    else if (e.type.match(/^spoiler$/)) {
+      accum += `<button type="button" class="btn btn-sm btn-warning btn-spoiler">Spoiler</button><p class="spoiler display-none">${pegAstToHtml(e.content)}</p>`
+    }
+    else if (e.type.match(/^(center|left|right)$/)) {
+      accum += `<p class="text-center">${pegAstToHtml(e.content)}</p>`
+    }
+    else if (e.type == "*") {
+      // must parse the inside for v2
+      //accum += `<li>${pegAstToHtml( bbcodePegParser.parse(e.unparse) )}</li>`
+      try_exit_li()
+      accum += `<li>`
+      in_li=true
+    }
+    else if (e.type == "error") {
+      accum += e.content
+    }
+    else if (e.content != null ){
+      accum += pegAstToHtml(e.content)
+    }
+    else {
+      accum += e
+    }
+    return accum
+  },"")
+  if (in_li) {
+    in_li=false
+    res +="</li>"
+  }
+  return res
+}
 
 function makePreview(txt) {
-  let html = bbCodeParser.parse(txt)
+  //dbg(pegAstToHtml(bbcodePegParser.parse(txt)))
+  // Faster, but less dynamic
+  //let html = bbCodeParser.parse(txt)
+  // Slower, but more dynamic
+  let html = pegAstToHtml(bbcodePegParser.parse(txt))
   let tmpl = document.createElement("div")
   tmpl.innerHTML = html
   return tmpl
@@ -159,7 +326,11 @@ function createPreviewCallbacks() {
     forum.parentElement.insertBefore(previewDiv,forum)
     let curDisplayedVersion = 0
     let nextVersion = 0
+    let updateTimeout
+    let updateTimeoutDelay = 50
     function UpdatePreview() {
+      // Measure load speed. Used for setting update delay dynamicly.
+      let startTime = Date.now()
       // Create a preview buffer
       let thisVersion = nextVersion++
       let newPreview = makePreview(textarea.value)
@@ -178,6 +349,11 @@ function createPreviewCallbacks() {
       // Wait for all images to load or error (size calculations needed) before we swap and rescroll
       // This is the part that actualy updates the preview
       Promise.all(imgLoadPromises).then(()=>{
+        let endTime = Date.now()
+        let updateLoadDelay = endTime - startTime
+        // average out the times
+        updateTimeoutDelay = (updateTimeoutDelay + updateLoadDelay) / 2
+        dbg(`It took ${updateLoadDelay} milli to update. Changing delay to ${updateTimeoutDelay} `)
         // Return if we are older than cur preview
         if (thisVersion < curDisplayedVersion) {
           newPreview.remove()
@@ -195,12 +371,17 @@ function createPreviewCallbacks() {
         $(document).scrollTop(old_scroll + $(document).height() - old_height);
       })
     }
+    function UpdatePreviewProxy() {
+      dbg(`Reseting timeout with delay ${updateTimeoutDelay} `)
+      clearTimeout(updateTimeout)
+      updateTimeout = setTimeout(UpdatePreview,updateTimeoutDelay)
+    }
     
     let buttons = Object.values(forum.querySelectorAll("button"))
     buttons.forEach((btn)=>{
-      btn.addEventListener('click', UpdatePreview)
+      btn.addEventListener('click', UpdatePreviewProxy)
     })
-    textarea.oninput = UpdatePreview
+    textarea.oninput = UpdatePreviewProxy
   })
 }
 
