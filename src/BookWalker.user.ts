4cc274c026261e888ba5c7dc218b379e96c2aa9f
Brandon Ray Beck <brandonrbeck101@gmail.com>
2019-07-17 10:08:16 -0500

    Catch more volume numbers

--- src/BookWalker.user.ts
+++ src/BookWalker.user.ts
@@ -5,7 +5,7 @@
 // @include    /^(?:https?:\/\/)?bookwalker\.jp\/de[a-zA-Z0-9]+-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]+(\/.*)?/
 // @include    /^(?:https?:\/\/)?bookwalker\.jp\/series\/\d+(\/.*)?/
 // @include    /^(?:https?:\/\/)?mangadex\.org\/title\/\d+(\/.*)?/
-// @version  0.1.30
+// @version  0.1.31
 // @grant unsafeWindow
 // @grant GM_xmlhttpRequest
 // ==/UserScript==
@@ -726,7 +726,7 @@ function createSingleInterface(serialData: SerialData): HTMLDivElement {
     && serialData.blob && serialData.mangadexId !== undefined) {
       const imageTypeMatch = serialData.blob.type.match(/^image\/(.+)/)
       let volumeMatch = toAsciiEquivilent(serialData.title).match(/\((\d+(?:\.\d+)?)\)$/)
-      if (!volumeMatch) volumeMatch = toAsciiEquivilent(serialData.title).match(/\s(\d+(?:\.\d+)?)$/)
+      if (!volumeMatch) volumeMatch = toAsciiEquivilent(serialData.title).match(/\D(\d+(?:\.\d+)?)$/)
       let volume: string | undefined
       if (volumeMatch) {
         volume = volumeMatch[1]
